56de91e49a6e32d97d383e3df2e1482621ca180b
Olegi Miami <zlatalove96@gmail.com>
2022-07-08 13:52:21 +0300

    add config system

---
+++ config.js
@@ -0,0 +1,83 @@
+function setFractionConfig() {
+    const nameTabs = document.getElementsByClassName("th clNames")
+    const nameTabArr = Array.from(nameTabs)
+
+    nameTabArr.map(tab => { tab.innerText = "Фракция" })
+
+    $('#orgBtnAddFraction').show()
+    $('#orgBtnAddFamily').hide()
+
+    $("#settingEms").val(1200)
+    $("#settingTech").val(1900)
+    $("#settingMilitary").val(2000)
+    $("#settingSuper").val(2500)
+
+    $('.clWin').each(function() { 
+        $(this).remove()
+    })
+
+    $('.clResult').each(function() { 
+        $(this).remove()
+    })
+}
+
+function setFamilyConfig() {
+    const nameTabs = document.getElementsByClassName("th clNames")
+    const nameTabArr = Array.from(nameTabs)
+    nameTabArr.map(tab => tab.innerText = "Семья")
+
+    const winTab = `<div class="th clWin">Победа</div>`
+    const win = `<div class="td clWin" id=sWin1"><span class="left"><select id="sWinCheckbox" onchange="calculateWin(1, this.selectedIndex)" ><option>╳</option><option>✔</option></select></span></div>`;
+
+    const resultTab = `<div class="th clResult">Итого</div>`
+    const result = `<div class="td clResult" id="sResult1"><span class="left"><input id="sResultVal1" type="text" class="left" value="0" disabled/></span></div>`
+
+    $("#settingEms").val(75000)
+    $("#settingTech").val(100000)
+    $("#settingMilitary").val(150000)
+    $("#settingSuper").val(200000)
+    $('#cover #tr0').append(winTab)
+    $('#cover #tr1').append(win)
+
+    $('#cover-results #tr0').append(resultTab)
+    $('#cover-results #tr1').append(result)
+
+    $('#orgBtnAddFraction').hide()
+    $('#orgBtnAddFamily').show()
+
+    $('#orgBtnAddFamily').click(function () {
+        $rows++;
+        $fractions++;
+        $btnDel = '<span class="sBtn mousehand" onclick="deleteFraction(' + $rows + ');">-</span>'
+        $btnUp = '<span class="sBtn btnSmall mousehand" onclick="upFraction(' + $rows + ');">▲</span>';
+        $btnDown = '<span class="sBtn btnSmall mousehand" onclick="downFraction(' + $rows + ');">▼</span>';
+        s = '<div class="tr" id="tr' + $rows + '">';
+        s = s + '<div class="td clNums" id="sNum' + $rows + '"><span class="sDigit">' + $rows + '</span>' + $btnDel + $btnUp + $btnDown + '</div>';
+        s = s + `<div class="td clNames" id="sName${$rows}"><span class="left"><input oninput="setFractionName(${$rows}, ${$rows})" id="sNameVal${$rows}" type="text" class="left" value=""/></span></div>`;
+        s = s + `<div class="td clEms" id="sEms${$rows}"><span class="left"><input oninput="calculate('sEmsVal${$rows}', 'rEmsVal${$rows}', ${$rows})" id="sEmsVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clTech" id="sTech${$rows}"><span class="left"><input oninput="calculate('sTechVal${$rows}', 'rTechVal${$rows}', ${$rows})" id="sTechVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clMilitary" id="sMilitary${$rows}"><span class="left"><input oninput="calculate('sMilitaryVal${$rows}', 'rMilitaryVal${$rows}', ${$rows})" id="sMilitaryVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clSuper" id="sSuper${$rows}"><span class="left"><input oninput="calculate('sSuperVal${$rows}', 'rSuperVal${$rows}', ${$rows})" id="sSuperVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clWin" id=sWin1"><span class="left"><select id="sWinCheckbox" onchange="calculateWin(${$rows}, this.selectedIndex)" ><option>╳</option><option>✔</option></select></span></div>`;
+        s = s + `</div>`;
+        $('#cover').append(s);
+
+        r = '<div class="tr" id="tr' + $rows + '">';
+        r = r + `<div class="td clNums" id="rNum${$rows}"><span class="sDigit2">${$rows}</span>${$btnDel}${$btnUp}${$btnDown}</div>`;
+        r = r + `<div class="td clNames" id="rName${$rows}"><span class="left"><input disabled type="text" id="rNameVal${$rows}" class="left" value=""/></span></div>`;
+        r = r + `<div class="td clEms" id="rEms${$rows}"><span class="left"><input id="rEmsVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clTech" id="rTech${$rows}"><span class="left"><input id="rTechVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clMilitary" id="rMilitary${$rows}"><span class="left"><input id="rMilitaryVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clSuper" id="rSuper${$rows}"><span class="left"><input id="rSuperVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clResult" id="sResult${$rows}"><span class="left"><input id="sResultVal${$rows}" type="text" class="left" value="0" disabled/></span></div>`
+        r = r + '</div>';
+
+        $('#cover-results').append(r)
+        renum()
+    })
+
+}
+
+function loadConfig() { 
+    $('#config').val() == 'Фракционный' ? setFractionConfig() : setFamilyConfig()
+}
--- fnc.js
+++ fnc.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $rows = $fractions = 1
     $select = '<input>'
+    $winSum = 0
 
     for ($i = 1; $i <= 3; $i++) { $("#content #tab" + $i).hide(); }
+    $('#orgBtnAddFamily').hide()
     $("#tabs li:first").attr("id", "current"); // Активируем первую закладку
     $("#content #tab1").fadeIn(); // Выводим содержание
 
@@ -18,27 +20,27 @@ $(document).ready(function () {
     $('#orgBtnAddFraction').click(function () {
         $rows++;
         $fractions++;
-        val = '0';
+        $val = '0';
         $btnDel = '<span class="sBtn mousehand" onclick="deleteFraction(' + $rows + ');">-</span>'
         $btnUp = '<span class="sBtn btnSmall mousehand" onclick="upFraction(' + $rows + ');">▲</span>';
         $btnDown = '<span class="sBtn btnSmall mousehand" onclick="downFraction(' + $rows + ');">▼</span>';
         s = '<div class="tr" id="tr' + $rows + '">';
         s = s + '<div class="td clNums" id="sNum' + $rows + '"><span class="sDigit">' + $rows + '</span>' + $btnDel + $btnUp + $btnDown + '</div>';
-        s = s + `<div class="td clNames" id="sName${$rows}"><span class="left"><input oninput="setFractionName(${$rows})" id="sNameVal${$rows}" type="text" class="left" value=""/></span></div>`;
-        s = s + `<div class="td clEms" id="sEms${$rows}"><span class="left"><input oninput="calculate('sEmsVal${$rows}', 'rEmsVal${$rows}')" id="sEmsVal${$rows}" type="text" class="left" value="${val}"/></span></div>`;
-        s = s + `<div class="td clTech" id="sTech${$rows}"><span class="left"><input oninput="calculate('sTechVal${$rows}', 'rTechVal${$rows}')" id="sTechVal${$rows}" type="text" class="left" value="${val}"/></span></div>`;
-        s = s + `<div class="td clMilitary" id="sMilitary${$rows}"><span class="left"><input oninput="calculate('sMilitaryVal${$rows}', 'rMilitaryVal${$rows}')" id="sMilitaryVal${$rows}" type="text" class="left" value="${val}"/></span></div>`;
-        s = s + `<div class="td clSuper" id="sSuper${$rows}"><span class="left"><input oninput="calculate('sSuperVal${$rows}', 'rSuperVal${$rows}')" id="sSuperVal${$rows}" type="text" class="left" value="${val}"/></span></div>`;
+        s = s + `<div class="td clNames" id="sName${$rows}"><span class="left"><input oninput="setFractionName(${$rows}, ${$rows})" id="sNameVal${$rows}" type="text" class="left" value=""/></span></div>`;
+        s = s + `<div class="td clEms" id="sEms${$rows}"><span class="left"><input oninput="calculate('sEmsVal${$rows}', 'rEmsVal${$rows}', ${$rows})" id="sEmsVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clTech" id="sTech${$rows}"><span class="left"><input oninput="calculate('sTechVal${$rows}', 'rTechVal${$rows}', ${$rows})" id="sTechVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clMilitary" id="sMilitary${$rows}"><span class="left"><input oninput="calculate('sMilitaryVal${$rows}', 'rMilitaryVal${$rows}', ${$rows})" id="sMilitaryVal${$rows}" type="text" class="left" value="0"/></span></div>`;
+        s = s + `<div class="td clSuper" id="sSuper${$rows}"><span class="left"><input oninput="calculate('sSuperVal${$rows}', 'rSuperVal${$rows}', ${$rows})" id="sSuperVal${$rows}" type="text" class="left" value="0"/></span></div>`;
         s = s + `</div>`;
         $('#cover').append(s);
 
         r = '<div class="tr" id="tr' + $rows + '">';
         r = r + `<div class="td clNums" id="rNum${$rows}"><span class="sDigit2">${$rows}</span>${$btnDel}${$btnUp}${$btnDown}</div>`;
         r = r + `<div class="td clNames" id="rName${$rows}"><span class="left"><input disabled type="text" id="rNameVal${$rows}" class="left" value=""/></span></div>`;
-        r = r + `<div class="td clEms" id="rEms${$rows}"><span class="left"><input id="rEmsVal${$rows}" disabled type="text" class="left" value="${val}"/></span></div>`;
-        r = r + `<div class="td clTech" id="rTech${$rows}"><span class="left"><input id="rTechVal${$rows}" disabled type="text" class="left" value="${val}"/></span></div>`;
-        r = r + `<div class="td clMilitary" id="rMilitary${$rows}"><span class="left"><input id="rMilitaryVal${$rows}" disabled type="text" class="left" value="${val}"/></span></div>`;
-        r = r + `<div class="td clSuper" id="rSuper${$rows}"><span class="left"><input id="rSuperVal${$rows}" disabled type="text" class="left" value="${val}"/></span></div>`;
+        r = r + `<div class="td clEms" id="rEms${$rows}"><span class="left"><input id="rEmsVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clTech" id="rTech${$rows}"><span class="left"><input id="rTechVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clMilitary" id="rMilitary${$rows}"><span class="left"><input id="rMilitaryVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
+        r = r + `<div class="td clSuper" id="rSuper${$rows}"><span class="left"><input id="rSuperVal${$rows}" disabled type="text" class="left" value="0"/></span></div>`;
         r = r + '</div>';
 
         $('#cover-results').append(r)
@@ -82,19 +84,28 @@ function setFractionName(id) {
     $('#rNameVal' + id).val($('#sNameVal' + id).val())
 }
 
-function calculate(first, second) {
+function calculate(first, second, id) {
     switch (second.replace(/[0-9]/g, '')) {
         case 'rEmsVal':
             $(`#${second}`).val($(`#${first}`).val() * $(`#settingEms`).val())
-            break;
+            $(`#sResultVal${id}`).val(parseInt($(`#rEmsVal${id}`).val()) + parseInt($(`#rTechVal${id}`).val()) + parseInt($(`#rSuperVal${id}`).val()) + parseInt($(`#rMilitaryVal${id}`).val()) + $winSum)
+            break
         case 'rTechVal':
             $(`#${second}`).val($(`#${first}`).val() * $(`#settingTech`).val())
-            break;
+            $(`#sResultVal${id}`).val(parseInt($(`#rEmsVal${id}`).val()) + parseInt($(`#rTechVal${id}`).val()) + parseInt($(`#rSuperVal${id}`).val()) + parseInt($(`#rMilitaryVal${id}`).val()) + $winSum)
+            break
         case 'rMilitaryVal':
             $(`#${second}`).val($(`#${first}`).val() * $(`#settingMilitary`).val())
-            break;
+            $(`#sResultVal${id}`).val(parseInt($(`#rEmsVal${id}`).val()) + parseInt($(`#rTechVal${id}`).val()) + parseInt($(`#rSuperVal${id}`).val()) + parseInt($(`#rMilitaryVal${id}`).val()) + $winSum)
+            break
         case 'rSuperVal':
             $(`#${second}`).val($(`#${first}`).val() * $(`#settingSuper`).val())
-            break;
+            $(`#sResultVal${id}`).val(parseInt($(`#rEmsVal${id}`).val()) + parseInt($(`#rTechVal${id}`).val()) + parseInt($(`#rSuperVal${id}`).val()) + parseInt($(`#rMilitaryVal${id}`).val()) + $winSum)
+            break
     }
+}
+
+function calculateWin(id, status) {
+    (status == 1) ? $winSum = 500000 : $winSum = 0
+    $(`#sResultVal1`).val($winSum + parseInt($(`#rEmsVal${id}`).val()) + parseInt($(`#rTechVal${id}`).val()) + parseInt($(`#rSuperVal${id}`).val()) + parseInt($(`#rMilitaryVal${id}`).val()))
 }
